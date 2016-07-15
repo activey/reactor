9bff400510878cdea4db5765d8672c65347e1747
grabslu <lukasz.grabski.ext@storaenso.com>
2016-07-15 21:28:48 +0200

    Introduced util method for creating string reactor response

--- buildall.bat
+++ buildall.bat
@@ -3,6 +3,7 @@ reactor-jenkins,^
 reactor-sonar,^
 reactor-spring-actuator,^
 reactor-system,^
+reactor-tfs,^
 reactor-transport-directinput,^
 reactor-transport-elastic-push,^
 reactor-transport-http-jetty,^
