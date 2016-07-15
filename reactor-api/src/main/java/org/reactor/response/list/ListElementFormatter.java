8aacfc6a9d4bdcc8002a7eab169f98cacf797319
grabslu <lukasz.grabski.ext@storaenso.com>
2016-07-15 21:28:08 +0200

    Introduced util method for creating string reactor response

--- reactor-api/src/main/java/org/reactor/response/list/ListElementFormatter.java
+++ reactor-api/src/main/java/org/reactor/response/list/ListElementFormatter.java
@@ -2,6 +2,5 @@ package org.reactor.response.list;
 
 public interface ListElementFormatter<T> {
 
-    public String formatListElement(long elementIndex, T listElement);
-
+    String formatListElement(long elementIndex, T listElement);
 }
